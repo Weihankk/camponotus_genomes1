f411fa88cebd5dfe9182df8f2bf9b105b2000285
Joe Manthey <jdmanthey@gmail.com>
2020-03-03 15:11:01 -0600

    Create 01_rename.sh

---
+++ 02_blochmannia/01_rename.sh
@@ -0,0 +1,7 @@
+# rename the samples for compatability with the cluster array jobs
+
+cd /lustre/scratch/jmanthey/23_camp1/00_fastq
+
+while read -r name1 name2; do
+	mv $name1 $name2
+done < rename_samples.txt
