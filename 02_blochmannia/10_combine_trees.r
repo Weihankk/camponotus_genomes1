4ba206d0408683f7aa96aa6043d28109743c8cbb
Joe Manthey <jdmanthey@gmail.com>
2020-03-06 15:37:10 -0600

    Create 10_combine_trees.r

---
+++ 02_blochmannia/10_combine_trees.r
@@ -0,0 +1,12 @@
+options(scipen=999)
+
+# list all the files in the trees directory
+x_files <- list.files(pattern="*tre", full.names=T)
+
+# trees into one file
+tree_list <- list()
+for(a in 1:length(x_files)) {
+	tree_list[[a]] <- scan(x_files[a], what="character")
+}
+tree_list <- unlist(tree_list)
+write(tree_list, file="camp_bloch_genes.trees", ncolumns=1)
