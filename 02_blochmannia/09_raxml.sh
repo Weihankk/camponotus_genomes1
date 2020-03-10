e2ab98f3c81a1a74d72b0b2cdf66b88df8da1e1f
Joe Manthey <jdmanthey@gmail.com>
2020-03-10 15:24:16 -0500

    Update 09_raxml.sh

--- 02_blochmannia/09_raxml.sh
+++ 02_blochmannia/09_raxml.sh
@@ -8,15 +8,16 @@
 #$ -P quanah
 #$ -l h_rt=12:00:00
 #$ -l h_vmem=12G
-#$ -t 1-542
+#$ -t 1-508
 
 # working directory = /lustre/scratch/jmanthey/23_camp1/07_unaligned_fasta
+# gene_list == ls -1 *aligned_trimmed* > gene_list.fasta
 
 fasta_array=$( head -n${SGE_TASK_ID} gene_list.fasta | tail -n1 )
 
-input_array=${fasta_array%.fasta}_aligned_trimmed.fasta
+input_array=${fasta_array}
 
-output_array=${fasta_array%.fasta}
+output_array=${fasta_array%_aligned_trimmed.fasta}
 
 raxmlHPC-PTHREADS-SSE3 -T 1 -f a -x 50 -m GTRGAMMA -p 253 -N 100 \
 -s /lustre/scratch/jmanthey/23_camp1/07_unaligned_fasta/${input_array} \
