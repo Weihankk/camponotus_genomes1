bcf3d00b54b35f8a3b58046d7e4a73c4cbe60e2d
Joe Manthey <jdmanthey@gmail.com>
2020-09-24 10:28:23 -0500

    Update 00_busco.sh

--- 01_genome_assembly/04_annotate_genes/00_busco.sh
+++ 01_genome_assembly/04_annotate_genes/00_busco.sh
@@ -1,10 +1,22 @@
-source activate busco
 
 cd /home/jmanthey/denovo_genomes
+
+#!/bin/sh
+#$ -V
+#$ -cwd
+#$ -S /bin/bash
+#$ -N camp_busco
+#$ -q omni
+#$ -pe sm 24
+#$ -P quanah
+#$ -l h_rt=48:00:00
+#$ -l h_vmem=8G
+
+source activate busco
 
 # before running busco edit and export the path to the config file
 export BUSCO_CONFIG_FILE="/home/jmanthey/busco/config.ini"
 
 run_busco --in /home/jmanthey/denovo_genomes/camp_sp_genome_filtered.fasta \
 --out  camp_busco --lineage_path /home/jmanthey/busco/hymenoptera_odb9/ \
---mode genome -sp human -c 16 
+--mode genome -sp human -c 24
