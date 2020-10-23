309a6570a75b7d2f0e1ea0a832ab19e9a30c35c8
Joe Manthey <jdmanthey@gmail.com>
2020-10-23 17:52:42 -0500

    Update 02_tblastn.sh

--- 02_blochmannia/13_pangenome/02_tblastn.sh
+++ 02_blochmannia/13_pangenome/02_tblastn.sh
@@ -4,13 +4,14 @@
 #$ -S /bin/bash
 #$ -N tblastn
 #$ -q omni
-#$ -pe sm 12
+#$ -pe sm 4
 #$ -P quanah
 #$ -l h_rt=48:00:00
 #$ -l h_vmem=8G
 
+export BATCH_SIZE=10
+
 makeblastdb -in blochmannia_sequences_all.fasta -dbtype  nucl -out bloch_all
 
-# output fields: qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore
 tblastn -query blochmania_AA_all.fasta -out blochmania_AA_all.blast -task tblastn -db bloch_all \
--outfmt 6 -max_target_seqs 100 -evalue 0.1 -num_threads 12
+-outfmt "6 qseqid sseqid pident length evalue" -max_target_seqs 100 -evalue 0.1 -num_threads 4
