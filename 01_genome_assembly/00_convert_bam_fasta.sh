1e8d2ec35050d03a24e587444cb97a81a1543cbf
Joe Manthey <jdmanthey@gmail.com>
2020-10-30 15:36:17 -0500

    Create 00_convert_bam_fasta.sh

---
+++ 01_genome_assembly/00_convert_bam_fasta.sh
@@ -0,0 +1,9 @@
+cd camponotus
+
+samtools fasta m54112_190614_222448.subreads.bam > camponotus_part1.pb.fasta
+
+samtools fasta m54112_190613_230143.subreads.bam > camponotus_part2.pb.fasta 
+
+samtools fasta m54112_190724_212023.subreads.bam > camponotus_part3.pb.fasta 
+
+samtools fasta m54112_190729_213922.subreads.bam > camponotus_part4.pb.fasta
